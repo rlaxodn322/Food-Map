88492d8e10487064991b0b1984e7e08ea7c10168
taewoo <rlaxodn322@gmail.com>
2023-06-20 16:05:27 +0900

    axios url 변경

--- front/axios-script.js
+++ front/axios-script.js
@@ -28,7 +28,7 @@ async function getDataSet(category) {
 
   const dataSet = await axios({
     method: "get", // http method
-    url: `http://localhost:3000/restaurants?category=${qs}`,
+    url: `http://43.200.38.212:3000/restaurants?category=${qs}`,
     headers: {}, // packet header
     data: {}, // packet body
   });
